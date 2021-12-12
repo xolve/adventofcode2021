2330d2196637f8ffae083ae7684114a96d18b213
xolve <xolve@protonmail.com>
2021-12-12 20:35:26 +0530

    Solutions for day 4.

---
+++ src/main/scala/net/xolve/adventofcode2021/GiantSquid.scala
@@ -0,0 +1,125 @@
+// https://adventofcode.com/2021/day/4
+
+package net.xolve.adventofcode2021
+
+import java.awt.print.Book
+import javax.management.ValueExp
+
+object GiantSquid {
+
+  def parseInputFile(filePath: String): (Seq[Int], Seq[BingoBoard]) = {
+    import scala.collection.mutable
+    import scala.io.Source
+    import scala.util.Using
+
+    Using(Source.fromFile(filePath)) { f =>
+      val lineIterator = f.getLines()
+      val randomNumbers = lineIterator.next().split(",").map(_.toInt).toSeq
+
+      println(lineIterator.next())
+
+      val boards = mutable.ListBuffer[BingoBoard]()
+
+      while (lineIterator.hasNext) {
+        val lines = lineIterator.takeWhile(_.nonEmpty).toSeq
+        require(lines.length == 5, s"${lines}")
+        boards addOne new BingoBoard(lines)
+      }
+
+      (randomNumbers, boards.toSeq)
+    }.get
+  }
+
+  class BingoBoard(lines: Seq[String]) {
+    val matrix: Array[Array[Cell]] = {
+      val matrix = lines.map { line =>
+        line
+          .split("\\s+")
+          .filterNot(_.isEmpty)
+          .map(n => Cell(n.toInt))
+      }.toArray
+
+      require(matrix.length == 5)
+      matrix.foreach(row => require(row.length == 5))
+      matrix
+    }
+
+    def markAndCheck(value: Int): Boolean = {
+      markCell(value)
+      crossedRow || crossedCol
+    }
+
+    def crossedRow: Boolean = {
+      matrix.exists(row => row.forall(_.crossed))
+    }
+
+    def crossedCol: Boolean = {
+      val rowSize = matrix.length
+      val colSize = matrix.head.length
+      (0 until colSize)
+        .map { colIdx =>
+          (0 until rowSize).map(rowIdx => matrix(rowIdx)(colIdx))
+        }
+        .exists(col => col.forall(_.crossed))
+    }
+
+    def markCell(value: Int): Unit = {
+      matrix foreach { row =>
+        row foreach { cell =>
+          if cell.value == value then cell.crossed = true
+        }
+      }
+    }
+
+    case class Cell(value: Int) {
+      var crossed: Boolean = false
+
+      override def toString: String = {
+        if crossed then String.format("%2s", "X") else String.format("%2d", value)
+      }
+    }
+
+    override def toString: String = {
+      matrix.map(row =>
+        row.map(cell => cell.toString).mkString(" ")
+      ).mkString("\n")
+    }
+  }
+
+  def part1(): Int = {
+    val (randomNumbers, boards) = parseInputFile("src/resources/input_day4.txt")
+    var crossedBoard: Option[BingoBoard] = None
+    val crossingNum = randomNumbers.find(num => {
+      crossedBoard = boards.find(_.markAndCheck(num))
+      crossedBoard.isDefined
+    })
+
+    (crossedBoard, crossingNum) match {
+      case (Some(board), Some(num)) => board.matrix.map(_.filterNot(_.crossed).map(_.value).sum).sum * num
+      case _ => throw new Exception("No rows or columns crossed :-(")
+    }
+  }
+
+  def part2(): Int = {
+    val (randomNumbers, boards) = parseInputFile("src/resources/input_day4.txt")
+    var crossedBoardNum: Option[(BingoBoard, Int)] = None
+    randomNumbers.foreach(num => {
+      boards
+        .filterNot(b => b.crossedRow || b.crossedCol)
+        .map(b => (b, num, b.markAndCheck(num)))
+        .findLast(_._3)
+        .map(t => (t._1, t._2))
+        .foreach(bn => crossedBoardNum = Some(bn))
+    })
+
+    crossedBoardNum match {
+      case Some(board, num) => board.matrix.map(_.filterNot(_.crossed).map(_.value).sum).sum * num
+      case _ => throw new Exception("No rows or columns crossed :-(")
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(part1())
+    println(part2())
+  }
+}
---
+++ src/test/scala/net/xolve/adventofcode2021/GiantSquidTests.scala
@@ -0,0 +1,99 @@
+package net.xolve.adventofcode2021
+
+import net.xolve.adventofcode2021.GiantSquid.BingoBoard
+import net.xolve.net.xolve.basetest.UnitSpec
+
+class GiantSquidTests extends UnitSpec {
+
+  val lines: Seq[String] =
+    """22 13 17 11  0
+      | 8  2 23  4 24
+      |21  9 14 16  7
+      | 6 10  3 18  5
+      | 1 12 20 15 19""".stripMargin.linesIterator.toSeq
+
+  val expected: Array[Array[Int]] = Array(
+    Array(22, 13, 17, 11, 0),
+    Array(8, 2, 23, 4, 24),
+    Array(21, 9, 14, 16, 7),
+    Array(6, 10, 3, 18, 5),
+    Array(1, 12, 20, 15, 19),
+  )
+
+  "BingoBoard" should {
+    "be well formed" when {
+      "created" in {
+        val board = new BingoBoard(lines)
+        board.matrix.zip(expected).foreach { ca =>
+          val cs = ca._1
+          val is = ca._2
+          cs.map(_.value) shouldBe is
+        }
+      }
+    }
+
+    "marked" when {
+      "number exists" in {
+        val board = new BingoBoard(lines)
+        board.markCell(16)
+        board.matrix(2)(3).crossed shouldBe true
+      }
+    }
+
+    "not marked" when {
+      "number not exists" in {
+        val board = new BingoBoard(lines)
+        board.markCell(99)
+        board.matrix foreach { row =>
+          row.forall(_.crossed == false)
+        }
+      }
+    }
+
+    "return true for crossed row" when {
+      "row is crossed" in {
+        val board = new BingoBoard(lines)
+        expected(2).foreach(board.markCell)
+        board.crossedRow shouldBe true
+      }
+    }
+
+    "return true for cross column" when {
+      "column is crossed" in {
+        val board = new BingoBoard(lines)
+        val col = Array(22, 8, 21, 6, 1)
+        col.foreach(board.markCell)
+        board.crossedCol shouldBe true
+      }
+    }
+
+    "markAndCheck" should {
+      "not return crossed" when {
+        "nothing is crossed" in {
+          val board = new BingoBoard(lines)
+          board.markAndCheck(22) shouldBe false
+          board.markAndCheck(9) shouldBe false
+          board.markAndCheck(16) shouldBe false
+          board.markAndCheck(10) shouldBe false
+          board.markAndCheck(15) shouldBe false
+        }
+      }
+
+      "return true for crossed row" when {
+        "row is crossed" in {
+          val board = new BingoBoard(lines)
+          expected(2).map(board.markAndCheck).last shouldBe true
+        }
+      }
+
+      "return true for crossed column" when {
+        "column is crossed" in {
+          val board = new BingoBoard(lines)
+          val expected = Array(22, 8, 21, 6, 1)
+          expected.map(board.markAndCheck).last shouldBe true
+        }
+      }
+    }
+  }
+
+}
