f381812ebe4d665a50b5c2424992395d3309514d
xolve <xolve@protonmail.com>
2021-12-20 01:58:06 +0530

    Solutions for day 6.

---
+++ src/main/scala/net/xolve/adventofcode2021/Lanternfish.scala
@@ -0,0 +1,87 @@
+// https://adventofcode.com/2021/day/6
+
+package net.xolve.adventofcode2021
+
+import scala.collection.mutable.ListBuffer
+
+object Lanternfish {
+
+  class Fish(initTimer: Int) {
+    require(initTimer >= 0 && initTimer <= 8)
+
+    private var _timer = initTimer
+    def timer: Int = _timer
+    private def timer_=(timer: Int): Unit = {
+      this._timer = timer
+    }
+
+    def increment(): Option[Fish] = {
+      val t = timer - 1
+      if t == -1 then
+        this.timer = 6
+        Some(new Fish(8))
+      else
+        timer = t
+        None
+    }
+
+    override def toString: String = s"Fish($timer)"
+  }
+
+  def parseInputFile(fileName: String): Seq[Fish] = {
+    import scala.io.Source
+    import scala.util.Using
+
+    Using(Source.fromFile(fileName)) { f =>
+      f.getLines()
+        .map { line =>
+          line.split(",").map(s => new Fish(s.toInt))
+        }
+        .toSeq
+    }.get.flatten
+  }
+
+  def part1(fishes: Seq[Fish], increments: Int): Seq[Fish] = {
+    import scala.collection.mutable
+
+    val fishesMut = (0 until increments).foldLeft(new ListBuffer[Fish] ++ fishes) { (fl, _) =>
+      val newFishes = fl.flatMap(_.increment())
+      fl :++ newFishes
+    }
+    fishesMut.toSeq
+  }
+
+  def part2(fishes: Seq[Int], increments: Int): Long = {
+    import scala.collection.mutable
+
+    val initialCounts: Map[Int, Long] = fishes.groupBy(i => i).map((k, v) => k -> v.length.toLong).toMap
+
+    val finalCounts = (0 until increments).foldLeft(initialCounts) { (fishesByTimerCounts, _) =>
+      val newCounts = new mutable.HashMap[Int, Long] withDefaultValue 0
+      fishesByTimerCounts.foreach((timer, count) =>
+        if timer == 0 then {
+          newCounts += (6 -> (newCounts(6) + count))
+          newCounts += (8 -> (newCounts(8) + count))
+        } else {
+          newCounts += ((timer - 1) -> (newCounts(timer - 1) + count))
+        }
+      )
+      newCounts.toMap
+    }
+
+    finalCounts.values.sum
+  }
+
+  def main(args: Array[String]): Unit = {
+    {
+      val fishes = parseInputFile("src/resources/input_day6.txt")
+      println(part1(fishes, 80).length)
+    }
+
+    {
+      val fishes = parseInputFile("src/resources/input_day6.txt").map(_.timer)
+      println(part2(fishes, 256))
+    }
+  }
+
+}
---
+++ src/test/scala/net/xolve/adventofcode2021/LanternfishTests.scala
@@ -0,0 +1,63 @@
+package net.xolve.adventofcode2021
+
+import net.xolve.net.xolve.basetest.UnitSpec
+
+class LanternfishTests extends UnitSpec {
+
+  import Lanternfish._
+
+  "Fish" should {
+    "produce another fish" when {
+      "day 6 is reached" in {
+        val fish = new Fish(3)
+
+        fish.increment() shouldBe None
+        fish.timer shouldBe 2
+
+        fish.increment() shouldBe None
+        fish.timer shouldBe 1
+
+        fish.increment() shouldBe None
+        fish.timer shouldBe 0
+
+        val expectedNewFish = fish.increment()
+        expectedNewFish.value shouldBe a [Fish]
+        expectedNewFish.value.timer shouldBe 8
+        fish.timer shouldBe 6
+      }
+    }
+  }
+
+  "part1" should {
+    "return correct answer" when {
+      "single fish" in {
+        val fishes = Seq(new Fish(3))
+        part1(fishes, 4).length shouldBe 2
+      }
+
+      "sample example" in {
+        val fishes = Seq(3, 4, 3, 1, 2).map(new Fish(_))
+        part1(fishes, 18).length shouldBe 26
+      }
+    }
+  }
+
+  "part2" should {
+    "return correct answer" when {
+      "single fish" in {
+        val fishes = Seq(3)
+        part2(fishes, 4) shouldBe 2
+      }
+
+      "multiple fish" in {
+        val fishes = Seq(3, 3, 3, 3, 3)
+        part2(fishes, 4) shouldBe 10
+      }
+
+      "sample example with 256 days" in {
+        val fishes = Seq(3, 4, 3, 1, 2)
+        part2(fishes, 256) shouldBe 26984457539L
+      }
+    }
+  }
+}
