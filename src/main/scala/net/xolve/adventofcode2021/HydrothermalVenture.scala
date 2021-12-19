3fc2cdb3be060168b1387dd4254ce319746bb62b
xolve <xolve@protonmail.com>
2021-12-19 18:19:19 +0530

    Solutions for day 5.

---
+++ src/main/scala/net/xolve/adventofcode2021/HydrothermalVenture.scala
@@ -0,0 +1,72 @@
+// https://adventofcode.com/2021/day/5
+
+package net.xolve.adventofcode2021
+
+object HydrothermalVenture {
+  case class Point(x: Int, y: Int)
+  case class Line(p0: Point, p1: Point)
+  object Line {
+    def create(p0: Point, p1: Point): Line = {
+      if p1.x < p0.x || (p1.x == p0.x && p1.y < p0.y) then {
+        Line(p1, p0)
+      } else {
+        Line(p0, p1)
+      }
+    }
+  }
+
+  def parseInputFile(file: String): Seq[Line] = {
+    import scala.io.Source
+    import scala.util.Using
+
+    Using(Source.fromFile(file)) { f =>
+      f.getLines().map {
+        case s"$x0,$y0 -> $x1,$y1" => Line.create(Point(x0.toInt, y0.toInt), Point(x1.toInt, y1.toInt))
+        case line => throw new IllegalArgumentException(s"$line is in invalid format.")
+      }
+      .toSeq
+    }.get
+  }
+
+  def part1(lines: Seq[Line]): Int = {
+    lines
+      .flatMap( line =>
+        if line.p0.x == line.p1.x then
+          (line.p0.y to line.p1.y).map(Point(line.p0.x, _))
+        else if line.p0.y == line.p1.y then
+          (line.p0.x to line.p1.x).map(Point(_, line.p0.y))
+        else
+          Seq()
+      )
+      .groupBy(p => p)
+      .count(_._2.length > 1)
+  }
+
+  def part2(lines: Seq[Line]): Int = {
+    lines
+      .flatMap( line =>
+        if line.p0.x == line.p1.x then
+          (line.p0.y to line.p1.y).map(Point(line.p0.x, _))
+        else if line.p0.y == line.p1.y then
+          (line.p0.x to line.p1.x).map(Point(_, line.p0.y))
+        else if line.p0.y < line.p1.y then
+          (line.p0.x to line.p1.x)
+            .zip((line.p0.y to line.p1.y))
+            .map((x, y) => Point(x, y))
+        else if line.p0.y > line.p1.y then
+          (line.p0.x to line.p1.x)
+            .zip((line.p0.y to line.p1.y by -1))
+            .map((x, y) => Point(x, y))
+        else
+          throw Exception(s"Cannot figure out line $line")
+      )
+      .groupBy(p => p)
+      .count(_._2.length > 1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val lines = parseInputFile("src/resources/input_day5.txt")
+    println(part1(lines))
+    println(part2(lines))
+  }
+}
---
+++ src/test/scala/net/xolve/adventofcode2021/HydrothermalVentureTests.scala
@@ -0,0 +1,38 @@
+package net.xolve.adventofcode2021
+
+import net.xolve.net.xolve.basetest.UnitSpec
+
+class HydrothermalVentureTests extends UnitSpec {
+
+  import HydrothermalVenture._
+
+  "part1" should {
+    "return correct answer" when {
+      "no line" in {
+        val lines = Seq()
+        HydrothermalVenture.part1(lines) shouldBe 0
+      }
+
+      "single line" in {
+        val lines = Seq(Line(Point(5, 0), Point(5, 10)))
+        HydrothermalVenture.part1(lines) shouldBe 0
+      }
+
+      "one crossing line" in {
+        val lines = Seq(
+          Line(Point(5, 0), Point(5, 10)),
+          Line(Point(3, 2), Point(12, 2)))
+        HydrothermalVenture.part1(lines) shouldBe 1
+      }
+
+      "Diagonal lines" in {
+        val lines = Seq(
+          Line(Point(5, 0), Point(5, 10)),
+          Line(Point(3, 2), Point(12, 2)),
+          Line(Point(2, 2), Point(10, 10)))
+        HydrothermalVenture.part1(lines) shouldBe 1
+      }
+    }
+  }
+
+}
