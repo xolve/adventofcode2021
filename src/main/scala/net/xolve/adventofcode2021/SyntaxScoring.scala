54d2c2e2d13d954300a3a9146b79823597d4eabe
xolve <xolve@protonmail.com>
2023-10-02 02:38:30 +0530

    Solved for day10 part2.

--- src/main/scala/net/xolve/adventofcode2021/SyntaxScoring.scala
+++ src/main/scala/net/xolve/adventofcode2021/SyntaxScoring.scala
@@ -2,6 +2,7 @@ package net.xolve.adventofcode2021
 
 object SyntaxScoring extends App:
 
+  import scala.collection.mutable
   import scala.util.boundary
   import scala.util.boundary.break
 
@@ -13,30 +14,63 @@ object SyntaxScoring extends App:
       fp.getLines().toList
     .get
 
-  private def firstUnmatchingChar(line: String): Option[Char] =
-    import scala.collection.mutable
+  private val unmatchedScoreMap = Map(
+    ')' -> 3,
+    ']' -> 57,
+    '}' -> 1197,
+    '>' -> 25137,
+  )
+
+  private def closingScoreMap = Map(
+    '(' -> 1,
+    '[' -> 2,
+    '{' -> 3,
+    '<' -> 4,
+  )
+
+  private def firstUnmatchingCharOrUnclosedString(line: String): Either[Char, List[Char]] =
     val stack = new mutable.Stack[Char]
     boundary:
       for c <- line do
         c match
           case '(' | '[' | '{' | '<' => stack.push(c)
-          case ')' => if stack.pop() != '(' then break(Some(')'))
-          case ']' => if stack.pop() != '[' then break(Some(']'))
-          case '}' => if stack.pop() != '{' then break(Some('}'))
-          case '>' => if stack.pop() != '<' then break(Some('>'))
-      None
+          case ')' => if stack.pop() != '(' then break(Left(')'))
+          case ']' => if stack.pop() != '[' then break(Left(']'))
+          case '}' => if stack.pop() != '{' then break(Left('}'))
+          case '>' => if stack.pop() != '<' then break(Left('>'))
+      Right(stack.toList)
 
   private def part1(lines: List[String]): Int =
-    val scoreMap = Map(
-      ')' -> 3,
-      ']' -> 57,
-      '}' -> 1197,
-      '>' -> 25137,
-    ) withDefaultValue 0
-    lines.flatMap(firstUnmatchingChar).map(scoreMap).sum
-  
+    lines.flatMap(firstUnmatchingCharOrUnclosedString(_).left.toOption).map(unmatchedScoreMap(_)).sum
+
+  private def part2(lines: List[String]): Long =
+    val sortedScores = lines.map(firstUnmatchingCharOrUnclosedString).filter(_.isRight).map { er =>
+        val leftOverString = er.getOrElse(List.empty)
+        leftOverString.foldLeft(0L)((acc, c) => acc * 5 + closingScoreMap(c))
+      }.sorted
+
+    sortedScores(sortedScores.length / 2)
+
+  {
+    val lines = List(
+        "[({(<(())[]>[[{[]{<()<>>",
+        "[(()[<>])]({[<{<<[]>>(",
+        "{([(<{}[<>[]}>{[]{[(<()>",
+        "(((({<>}<{<{<>}{[]{[]{}",
+        "[[<[([]))<([[{}[[()]]]",
+        "[{[{({}]{}}([{[{{{}}([]",
+        "{<[[]]>}<{[{[{[]{()[[[]",
+        "[<(<(<(<{}))><([]([]()",
+        "<{([([[(<>()){}]>(<<{{",
+        "<{([{{}}[<[[[<>{}]]]>[]]",
+    )
+    println(part1(lines))
+    println(part2(lines))
+  }
+
   {
     val input = readInput()
     println(s"Part 1: ${part1(input)}")
+    println(s"Part 2: ${part2(input)}")
   }
 end SyntaxScoring
